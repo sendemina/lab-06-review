0d77b706618ff34e65e882a1cd3dcf390f319da3
Kseniia Demina <Kseniia@kseniia-c02wv1hlhtdd.lan>
2021-10-18 16:57:14 -0400

    bmi calculator in python added

---
+++ bmi.py
@@ -0,0 +1,8 @@
+import sys
+print("BMI: Body Mass Index Calculator")
+userWeight = input("Enter your weight in pounds: ")
+userHeight = input("Enter your height in inches: ")
+
+myBMI = (703 * float(userWeight)) / (float(userHeight) ** 2)
+myBMI = round(myBMI, 2)
+print("Your body mass index (BMI) is " + str(myBMI) + "%")
