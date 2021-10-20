009215ffbce0bc66b49d6e8c4af356754149b84d
Kseniia Demina <Kseniia@kseniia-c02wv1hlhtdd.lan>
2021-10-20 15:57:34 -0400

    finished the shout.html

--- bmi.py
+++ bmi.py
@@ -3,6 +3,7 @@ print("BMI: Body Mass Index Calculator")
 userWeight = input("Enter your weight in pounds: ")
 userHeight = input("Enter your height in inches: ")
 
+# myBMI = (703 * float(userWeight)) / (float(userHeight) * float(userheight))
 myBMI = (703 * float(userWeight)) / (float(userHeight) ** 2)
 myBMI = round(myBMI, 2)
 print("Your body mass index (BMI) is " + str(myBMI) + "%")
